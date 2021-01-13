e8b0981e3a4c09c546c50afdbe5ac1f1d98783da
Abuzar Aslam <abuzaraslam96@gmail.com>
2021-01-13 18:12:36 +0800

    CAT: Added the dependencies and base for Koin Dependency injection

---
+++ app/src/main/java/com/abuzar/catastrophic/base/App.kt
@@ -0,0 +1,13 @@
+package com.abuzar.catastrophic.base
+
+import android.app.Application
+import com.abuzar.catastrophic.injection.viewModelModule
+import org.koin.android.ext.android.startKoin
+
+class App :Application(){
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin(applicationContext, listOf(viewModelModule))
+    }
+}
---
+++ app/src/main/java/com/abuzar/catastrophic/injection/AppModule.kt
@@ -0,0 +1,8 @@
+package com.abuzar.catastrophic.injection
+
+import org.koin.dsl.module.module
+
+val viewModelModule = module {
+
+
+}
