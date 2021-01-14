3d9aa6b431d91f6d3856229ac4fd3b2ef952f091
Abuzar Aslam <abuzaraslam96@gmail.com>
2021-01-14 22:18:11 +0800

    CAT: Updating the CAT useCase and ApiInterface ...

--- app/src/main/java/com/abuzar/catastrophic/data/CatApi.kt
+++ app/src/main/java/com/abuzar/catastrophic/data/CatApi.kt
@@ -1,5 +1,13 @@
 package com.abuzar.catastrophic.data
 
+import com.abuzar.catastrophic.data.model.CatModel
+import io.reactivex.Observable
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface CatApi {
+
+    @GET("v1/images/search")
+    fun fetchCatApi(@Query("limit") limit: Int,@Query("page") page : Int, @Query("mime_types") type : String, @Query("order")order : String): Observable<CatModel>
 
 }
--- app/src/main/java/com/abuzar/catastrophic/interactors/CatUseCase.kt
+++ app/src/main/java/com/abuzar/catastrophic/interactors/CatUseCase.kt
@@ -5,10 +5,10 @@ import com.abuzar.catastrophic.data.model.CatModel
 import io.reactivex.Observable
 
 class CatUseCase(private val catApi: CatApi) : BaseUseCase<CatModel>() {
-    
+
 
     override fun buildUseCaseObservable(): Observable<CatModel> {
-        TODO("Not yet implemented")
+        return catApi.fetchCatApi(20,1,"png","Desc")
     }
 
 }
