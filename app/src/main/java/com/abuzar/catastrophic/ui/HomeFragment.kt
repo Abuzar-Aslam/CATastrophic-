761a330a01a328ecc55f12399ad350068b874497
Abuzar Aslam <abuzaraslam96@gmail.com>
2021-01-15 16:59:07 +0800

    CAT: Added the Fragment, Viewmodel and layout file for HomeFragment

---
+++ app/src/main/java/com/abuzar/catastrophic/ui/HomeFragment.kt
@@ -0,0 +1,12 @@
+package com.abuzar.catastrophic.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+class HomeFragment :Fragment(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
---
+++ app/src/main/java/com/abuzar/catastrophic/ui/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.abuzar.catastrophic.ui
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel: ViewModel(){
+    
+}
